6abeea1d566c72514122791e7384e253b0125121
Aal Emraan <developer.aalemraan@gmail.com>
2021-10-12 10:53:29 +0600

    first commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA58RyLZYzjXo3zSaubHHbUzSAsVrkOdm4",
+    authDomain: "ema-john-31146.firebaseapp.com",
+    projectId: "ema-john-31146",
+    storageBucket: "ema-john-31146.appspot.com",
+    messagingSenderId: "530099863739",
+    appId: "1:530099863739:web:a8808e9531353a9f7f699e"
+  };
+
+  export default firebaseConfig;
